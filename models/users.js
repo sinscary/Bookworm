68ca1fd81de4037e322657a0a42cff8053001494
sinscary <nbsharma@outlook.com>
2016-10-12 14:17:55 +0530

    Add bcrypt to hash password

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -23,5 +24,20 @@ var UserSchema = new mongoose.Schema({
 
 });
 
+//hash password before saving to db
+
+UserSchema.pre('save', function(next){
+	var user = this;
+	bcrypt.hash(user.password, 10, function(err, hash){
+		if(err){
+			return next(err);
+		}
+		user.password = hash;
+		next();
+	})
+});
+
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
 
 //GET/Register
 router.get('/register', function(req, res, next){
@@ -8,7 +9,41 @@ router.get('/register', function(req, res, next){
 
 //POST/Register
 router.post('/register', function(req, res, next){
-	return res.send('user created');
+	if(req.body.email &&
+	   req.body.name &&
+	   req.body.favoriteBook &&
+	   req.body.password &&
+	   req.body.confirmPassword){
+
+
+	   //Check if passwords match
+	   if(req.body.password !== req.body.confirmPassword){
+	   	var err = new Error('Password do not match');
+	   	err.status=400;
+	   	return next(err);
+	   }
+	   // create objects with form input data
+	   var userData = {
+	   		email: req.body.email,
+	   		name: req.body.name,
+	   		favoriteBook: req.body.favoriteBook,
+	   		password: req.body.password
+	   };
+
+	   User.create(userData, function(error, user){
+	   	if(error){
+	   		return next(error);
+	   	}
+	   	else{
+	   		return res.redirect('/profile');
+	   	}
+	   })
+	} 
+	else{ //Error if any field is left empty
+		var err = new Error('All fields are required');
+		err.status = 400;
+		return next(err);
+	}
 });
 
 // GET /
