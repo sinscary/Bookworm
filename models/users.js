6f0ee1512d7ca8a61351898ca5c788f8c00fcf03
sinscary <nbsharma@outlook.com>
2016-10-12 17:16:51 +0530

    Add logout functionality and session handling to website

--- models/users.js
+++ models/users.js
@@ -24,6 +24,29 @@ var UserSchema = new mongoose.Schema({
 
 });
 
+//Authenticate method
+UserSchema.statics.authenticate = function(email, password, callback){
+	User.findOne({email: email})
+		.exec(function(error, user){
+			if(error){
+				return callback(error);
+			}
+			else if(!user){
+				var err = new Error('user not found');
+				err.status = 401;
+				return callback(err);
+			}
+			bcrypt.compare(password, user.password, function(error, result){
+				if(result==true){
+					return callback(null, user);
+				}
+				else{
+					return callback();
+				}
+			})
+		});
+}
+
 //hash password before saving to db
 
 UserSchema.pre('save', function(next){
